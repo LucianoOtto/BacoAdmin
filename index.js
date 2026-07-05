a051cf2c0831ac535e38ef0b8c8c5341d7671a19
LucianoOtto <lucianootto11@gmail.com>
2026-07-05 16:59:05 -0300

    Fix: Agregar requireTLS y configuracion de ciphers para evitar timeout

--- index.js
+++ index.js
@@ -30,7 +30,6 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
-app.options(cors(corsOptions));
 
 const compradoresPath = path.join(__dirname, 'compradores.json');
 const rrppPath = path.join(__dirname, 'rrpp.json'); 
